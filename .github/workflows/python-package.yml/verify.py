f2f7d6fcc1922688a67188222d60b4234c91ec98
AphyXia404 <74092486+AphyXia404@users.noreply.github.com>
2020-11-07 15:33:36 +0700

    Create verify.py

---
+++ .github/workflows/python-package.yml/verify.py
@@ -0,0 +1,7 @@
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+print("===================angka acak===================")
+print("nomor anda adalah")
+print(random.randint(1,10))
